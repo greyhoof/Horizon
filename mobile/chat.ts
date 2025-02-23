4ad5867f021a6b93621ce242bc9c6b110f111915
Sylvia Roselie <rose@rosely.me>
2025-02-22 21:14:24 -0500

    Fix mobile node build

--- mobile/chat.ts
+++ mobile/chat.ts
@@ -37,6 +37,7 @@ import Connection from '../fchat/connection';
 import {appVersion, Logs, SettingsStore} from './filesystem';
 import Index from './Index.vue';
 import Notifications from './notifications';
+import { GeneralSettings } from '../electron/common';
 
 const version = (<{version: string}>require('./package.json')).version; //tslint:disable-line:no-require-imports
 (<any>window)['setupPlatform'] = (platform: string) => { //tslint:disable-line:no-any
@@ -47,7 +48,7 @@ if(process.env.NODE_ENV === 'production')
     setupRaven('https://a9239b17b0a14f72ba85e8729b9d1612@sentry.f-list.net/2', `mobile-${version}`);
 
 const connection = new Connection('F-Chat 3.0 (Mobile)', appVersion, Socket);
-initCore(connection, Logs, SettingsStore, Notifications);
+initCore(connection, new GeneralSettings(), Logs, SettingsStore, Notifications);
 
 new Index({ //tslint:disable-line:no-unused-expression
     el: '#app'
--- mobile/webpack.config.js
+++ mobile/webpack.config.js
@@ -1,64 +1,89 @@
-const path = require('path');
-const ForkTsCheckerWebpackPlugin = require('@f-list/fork-ts-checker-webpack-plugin');
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
-const vueTransformer = require('@f-list/vue-ts/transform').default;
+const path = require("path");
+const ForkTsCheckerWebpackPlugin = require("@f-list/fork-ts-checker-webpack-plugin");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+const { Stream } = require("stream");
+const { node } = require("webpack");
+const vueTransformer = require("@f-list/vue-ts/transform").default;
+const nodepolyfillplugin = require("node-polyfill-webpack-plugin");
 
 const config = {
-    entry: {
-        chat: [__dirname + '/chat.ts', __dirname + '/index.html']
-    },
-    output: {
-        path: __dirname + '/www',
-        filename: '[name].js'
-    },
-    context: __dirname,
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                loader: 'ts-loader',
-                options: {
-                    appendTsSuffixTo: [/\.vue$/],
-                    configFile: __dirname + '/tsconfig.json',
-                    transpileOnly: true,
-                    getCustomTransformers: () => ({before: [vueTransformer]})
-                }
-            },
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader',
-                options: {
-                    compilerOptions: {
-                        preserveWhitespace: false
-                    }
-                }
-            },
-            {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader'},
-            {test: /\.(woff2?)$/, loader: 'file-loader'},
-            {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader'},
-            {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader'},
-            {test: /\.(wav|mp3|ogg)$/, loader: 'file-loader?name=sounds/[name].[ext]'},
-            {test: /\.(png|html)$/, loader: 'file-loader?name=[name].[ext]'},
-            {test: /(?<!\.vue)\.scss/, use: ['css-loader', 'sass-loader']},
-            {test: /\.vue\.scss/, loader: ['vue-style-loader','css-loader','sass-loader']},
-            {test: /\.vue\.css/, loader: ['vue-style-loader','css-loader']},
-        ]
-    },
-    plugins: [
-        new ForkTsCheckerWebpackPlugin({async: false, vue: true, tslint: path.join(__dirname, '../tslint.json')}),
-        new VueLoaderPlugin()
+  entry: {
+    chat: [__dirname + "/chat.ts", __dirname + "/index.html"],
+  },
+  output: {
+    path: __dirname + "/www",
+    filename: "[name].js",
+  },
+  context: __dirname,
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: "ts-loader",
+        options: {
+          appendTsSuffixTo: [/\.vue$/],
+          configFile: __dirname + "/tsconfig.json",
+          transpileOnly: true,
+          getCustomTransformers: () => ({ before: [vueTransformer] }),
+        },
+      },
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+        options: {
+          compilerOptions: {
+            preserveWhitespace: false,
+          },
+        },
+      },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader" },
+      { test: /\.(woff2?)$/, loader: "file-loader" },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader" },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader" },
+      {
+        test: /\.(wav|mp3|ogg)$/,
+        loader: "file-loader",
+        options: { name: "sounds/[name].[ext]" },
+      },
+      {
+        test: /\.(png|html)$/,
+        loader: "file-loader",
+        options: { name: "[name].[ext]" },
+      },
+      { test: /(?<!\.vue)\.scss/, use: ["css-loader", "sass-loader"] },
+      {
+        test: /\.vue\.scss/,
+        use: ["vue-style-loader", "css-loader", "sass-loader"],
+      },
+      { test: /\.vue\.css/, use: ["vue-style-loader", "css-loader"] },
     ],
-    resolve: {
-        'extensions': ['.ts', '.js', '.vue', '.scss']
-    }
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+      vue: true,
+      tslint: path.join(__dirname, "../tslint.json"),
+    }),
+    new VueLoaderPlugin(),
+  ],
+  resolve: {
+    fallback: {
+      fs: false,
+      tls: false,
+      net: false,
+    },
+    extensions: [".ts", ".js", ".vue", ".scss"],
+  },
 };
 
-module.exports = function(mode) {
-    if(mode === 'production') {
-        process.env.NODE_ENV = 'production';
-        config.devtool = 'source-map';
-    } else {
-        config.devtool = 'none';
-    }
-    return config;
+config.plugins.push(new nodepolyfillplugin());
+
+module.exports = function (mode) {
+  if (mode === "production") {
+    process.env.NODE_ENV = "production";
+    config.devtool = "source-map";
+  } else {
+    config.devtool = false;
+  }
+  return config;
 };
