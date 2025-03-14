dfb964a33e57136604ff7f79f1210ff34929618f
Sylvia Roselie <rose@rosely.me>
2025-03-13 21:54:11 -0400

    chore: update Sass dependencies and refactor color functions
    - Replaced `sass` with `sass-embedded` in package.json and updated version to 1.85.1.
    - Updated `sass-loader` to version 16.0.5.
    - Refactored SCSS files to use `sass:color` and `sass:math` for color manipulation functions, replacing deprecated functions like `lighten` and `darken` with `color.scale`.
    - Removed the Snyk policy file as it is no longer needed.

--- scss/importer.js
+++ scss/importer.js
@@ -1,14 +1,13 @@
 const path = require('path');
 const fs = require('fs');
 
-module.exports = function importer(url) {
+module.exports = function(url, prev, done) {
   if (url[0] === '~') {
     const file = url.substr(1);
     for (const base of require.resolve.paths('')) {
       const filePath = path.resolve(base, file);
       if (fs.existsSync(path.dirname(filePath))) {
-        url = filePath;
-        break;
+        return { file: filePath };
       }
     }
   }
