474a6b677e737e984d6d4a22fc762c57103929ce
Sylvia Roselie <rose@rosely.me>
2025-03-13 21:54:48 -0400

    style: prettier lint

--- scss/importer.js
+++ scss/importer.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 
-module.exports = function(url, prev, done) {
+module.exports = function (url, prev, done) {
   if (url[0] === '~') {
     const file = url.substr(1);
     for (const base of require.resolve.paths('')) {
