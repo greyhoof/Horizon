8be3f179e614dfd35262a18b0e48be35ce3d2a3b
Sylvia Roselie <rose@rosely.me>
2025-10-04 08:35:23 -0400

    feat(exporter): Add exporter.{html,ts}
    Authored by:
    Rose (rose@rosely.me)

---
+++ electron/exporter.ts
@@ -0,0 +1,33 @@
+import * as qs from 'querystring';
+import log from 'electron-log'; //tslint:disable-line:match-default-export-name
+
+import { GeneralSettings } from './common';
+import ExporterWindow from './Exporter.vue';
+
+log.info('init.exporter');
+
+const params = <{ [key: string]: string | undefined }>(
+  qs.parse(window.location.search.slice(1))
+);
+const settings = Object.assign(
+  new GeneralSettings(),
+  JSON.parse(params['settings']!)
+);
+const importHint = params['import'];
+
+const logLevel = process.env.NODE_ENV === 'production' ? 'info' : 'silly';
+
+log.transports.file.level = settings.risingSystemLogLevel || logLevel;
+log.transports.console.level = settings.risingSystemLogLevel || logLevel;
+log.transports.file.maxSize = 5 * 1024 * 1024;
+
+log.info('init.exporter.vue');
+
+const exporterApp = new ExporterWindow({
+  el: '#exporterWindow',
+  data: { settings, importHint }
+});
+
+log.debug('init.exporter.vue.done');
+
+export default exporterApp;
