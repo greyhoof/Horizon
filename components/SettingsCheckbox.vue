3e8de580b7c3f4d01d6f434833025e027c636f15
FatCatClient <82377182+FatCatClient@users.noreply.github.com>
2025-10-16 18:48:17 +0200

    feat: Adds generic components for toggling settings

---
+++ components/SettingsCheckbox.vue
@@ -0,0 +1,49 @@
+<template>
+  <div
+    class="btn-group"
+    role="group input-group"
+    aria-label="Basic radio toggle button group"
+  >
+    <input
+      type="radio"
+      class="btn-check form-control"
+      :name="name"
+      :id="`${name}-true`"
+      autocomplete="off"
+      :title="l('conversationSettings.true')"
+      :checked="value === true"
+      @change="$emit('input', true)"
+    />
+    <label class="btn btn-outline-success" :for="`${name}-true`">
+      <i class="fa-solid fa-check"></i>
+    </label>
+
+    <input
+      type="radio"
+      class="btn-check"
+      :name="name"
+      :id="`${name}-false`"
+      autocomplete="off"
+      :title="l('conversationSettings.false')"
+      :checked="value === false"
+      @change="$emit('input', false)"
+    />
+    <label class="btn btn-outline-danger" :for="`${name}-false`">
+      <i class="fa-solid fa-xmark"></i>
+    </label>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import l from '../chat/localize';
+
+  interface Props {
+    value: boolean;
+    name: string;
+  }
+
+  defineProps<Props>();
+  defineEmits<{
+    input: [value: boolean];
+  }>();
+</script>
